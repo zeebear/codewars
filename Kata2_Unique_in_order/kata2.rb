4e9d77b101defab08c213fa0a2fb7c341622f4b7
Claire Fahy <zeebear21@gmail.com>
2019-03-14 22:45:48 -0300

    done in less than 15 mins

--- Kata2_Unique_in_order/kata2.rb
+++ Kata2_Unique_in_order/kata2.rb
@@ -1,3 +1,8 @@
 def unique_in_order(sequence)
-
+  sequence = sequence.split('') if sequence.is_a?(String)
+  sequence_unique = []
+  sequence.each_with_index do |char, ind|
+    sequence_unique << char unless char == sequence[ind - 1] && ind > 0
+  end
+  sequence_unique
 end
